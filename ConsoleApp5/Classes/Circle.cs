f3bdc18d5bea118fa953ac0e8893afe610bedcaf
vitalik <venhrynyak@gmail.com>
2018-12-02 13:04:41 +0200

    Move classes to new folder Classes

---
+++ ConsoleApp5/Classes/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Circle
+    {
+        public Point Center { get; set; }
+        public int Radius { get; set; }
+
+    }
+}
