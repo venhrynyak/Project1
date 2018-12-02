6932cd9cab02171fca8ac40a6935643686d73994
vitalik <venhrynyak@gmail.com>
2018-12-02 13:01:58 +0200

    Add class Line amd Rectangle

---
+++ ConsoleApp5/Line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Line
+    {
+        public Point Start { get; set; }
+        public Point End { get; set; }
+
+
+
+    }
+}
---
+++ ConsoleApp5/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Rectangle
+    {
+        Point[] points;
+
+        public Rectangle(Point[] points)
+        {
+            this.points = points;
+        }
+
+
+    }
+}
