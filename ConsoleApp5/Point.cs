a897385ef6f623222b8bbaf228b54c4753de1f3d
vitalik <venhrynyak@gmail.com>
2018-12-02 12:56:44 +0200

    Add property Z to class Point

--- ConsoleApp5/Point.cs
+++ ConsoleApp5/Point.cs
@@ -10,12 +10,14 @@ namespace ConsoleApp5
     {
         public int X { get; set; }
         public int Y { get; set; }
+        public int Z { get; set; }
 
 
-        public Point(int x, int y)
+        public Point(int x, int y, int z)
         {
             X = x;
             Y = y;
+            Z = z;
         }
     }
 }
