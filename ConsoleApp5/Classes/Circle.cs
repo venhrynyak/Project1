9847aae6d75145e104e0b80052e3b0875be7d291
vitalik <venhrynyak@gmail.com>
2018-12-02 13:22:24 +0200

    Add constructor to class Circle

--- ConsoleApp5/Classes/Circle.cs
+++ ConsoleApp5/Classes/Circle.cs
@@ -11,5 +11,16 @@ namespace ConsoleApp5
         public Point Center { get; set; }
         public int Radius { get; set; }
 
+
+
+        public Circle(Point center, int radius)
+        {
+            this.Center = center;
+            this.Radius = radius;
+        }
+
+
+
+
     }
 }
